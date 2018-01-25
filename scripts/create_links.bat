dad295865cf04103039625131a35ae0b6a5d31a8
Kristian Evers <kristianevers@gmail.com>
2018-01-25 11:32:35 +0100

    Added windows install scripts. Creates hardlinks from repository folder to C:\OSGeo4W64\share\proj.

---
+++ scripts/create_links.bat
@@ -0,0 +1,13 @@
+set PROJ_LIB=C:\OSGeo4W64\share\proj
+
+mklink /H %PROJ_LIB%\DK \..\resources\DK
+mklink /H %PROJ_LIB%\NKG ..\resources\NKG
+mklink /H %PROJ_LIB%\GL ..\resources\GL
+mklink /H %PROJ_LIB%\FO ..\resources\FO
+mklink /H %PROJ_LIB%\dvr90g2013.gtx ..\resources\dvr90g2013.gtx
+mklink /H %PROJ_LIB%\fvr09g2012.gtx ..\resources\fvr09g2012.gtx
+mklink /H %PROJ_LIB%\nkgrf03vel_realigned_xy.ct2 ..\resources\nkgrf03vel_realigned_xy.ct2
+mklink /H %PROJ_LIB%\nkgrf03vel_realigned_z.gtx ..\resources\nkgrf03vel_realigned_z.gtx
+mklink /H %PROJ_LIB%\s34j.pol ..\resources\s34j.pol
+mklink /H %PROJ_LIB%\s34s.pol ..\resources\s34s.pol
+mklink /H %PROJ_LIB%\s45b.pol ..\resources\s45b.pol
